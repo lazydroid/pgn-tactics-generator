a84cc156fa4c999b3bbefd902c0d5deaa76af779
lenik terenin <lenik@lazydroid.com>
2020-12-19 01:58:33 +0900

    refactored downloading games for a user

--- download_games.py
+++ download_games.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python
-
-"""Downloading chess puzzles for lichess.org"""
-
+# encoding: utf-8
+#
+# Downloading chess puzzles for lichess.org
+#
 import argparse
 import chess
 import chess.pgn
@@ -12,25 +13,29 @@ import requests
 import chess
 import re
 import time
+import datetime
 
 parser = argparse.ArgumentParser(description=__doc__)
 parser.add_argument("--token", metavar="TOKEN",default="",
-                    help="secret token for the lichess api")
+					help="secret token for the lichess api")
 parser.add_argument("username", metavar="USERNAME",
-                    help="Username in lichess")
+					help="Username in lichess")
 parser.add_argument("--quiet", dest="loglevel",
-                    default=logging.DEBUG, action="store_const", const=logging.INFO,
-                    help="substantially reduce the number of logged messages")
+					default=logging.DEBUG, action="store_const", const=logging.INFO,
+					help="substantially reduce the number of logged messages")
 parser.add_argument("--max", metavar="MAX",default="60",
-                    help="max number of games")
+					help="max number of games")
 settings = parser.parse_args()
 logging.basicConfig(format="%(message)s", level=settings.loglevel, stream=sys.stdout)
 
-logging.debug("Downloading games from: "+settings.username)
+logging.debug("Downloading games for: "+settings.username)
 
 response = requests.get('https://lichess.org/api/games/user/'+settings.username+'?max='+settings.max+'&token=' + settings.token+'&perfType=blitz,rapid,classical&opening=true')
-pgn = str(response.text)
-all_games = open("games.pgn", "w")
-all_games.write(pgn)
-all_games.close()
-logging.debug("Finished. Pgn is in games.pgn ")
+#pgn = str(response.text)
+
+now = datetime.datetime.now()
+suffix = now.strftime( '%Y-%m-%d' )
+with open("%s_%s_games.pgn" % (settings.username, suffix), "w") as all_games :
+	all_games.write(response.text.encode('utf-8'))
+
+logging.debug("Finished.")
